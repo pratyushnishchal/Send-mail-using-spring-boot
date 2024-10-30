49dfc2c2b6c560d69dd8c26250a160b2a7e1e0c7
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-10-30 18:45:48 +0530

    controller layer code

--- controller/MailSendController.java
+++ controller/MailSendController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.sendmail.send_email.service.EmailService;
 
 @RestController
